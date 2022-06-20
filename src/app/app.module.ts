f46cef6732c8c9c2f65be70f65e9eb60832a054f
Johan Steiner Roug <johst@orsted.dk>
2022-06-20 23:12:55 +0200

    no message

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ScorecardComponent } from './scorecard/scorecard.component';
